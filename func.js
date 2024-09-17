58306a1b84c037d50c877f5b8e447f72cc9b91dc
lior <liorneman23@gmail.com>
2024-09-17 20:41:31 +0300

    funcs

---
+++ func.js
@@ -0,0 +1,8 @@
+
+export const  add=()=>{
+
+    console.log("david");
+
+
+}
+
--- server.js
+++ server.js
@@ -1,2 +1,8 @@
+import { add } from './func.js';
+
+
+add();
 console.log("welcome git");
 console.log("welcome ");
+
+
